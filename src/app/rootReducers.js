ddef553ab077401da0a27f71a60b208fc4e09fbb
Claudio Flocco <claudio@DESKTOP-HA26NJA.localdomain>
2021-06-30 17:01:31 +0200

    created the dedicated slice for the cart

--- src/app/rootReducers.js
+++ src/app/rootReducers.js
@@ -1,8 +1,9 @@
-import { combineReducers } from "@reduxjs/toolkit";
-import itemSliceReducers from "../features/itemListSlice";
-
+import { combineReducers } from '@reduxjs/toolkit'
+import itemSliceReducers from '../features/itemListSlice'
+import cartSliceReducers from '../features/cartSlice'
 const rootReducers = combineReducers({
   itemsData: itemSliceReducers,
-});
+  cartData: cartSliceReducers,
+})
 
-export default rootReducers;
+export default rootReducers
--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import styled from 'styled-components'
+// import styled from 'styled-components'
 import { fetchItemsList } from '../features/itemListSlice'
 
 import Container from './Container'
