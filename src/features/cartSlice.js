6916f23f2a1381b6b165cc34198b2c70394cbc96
Claudio <claudio@DESKTOP-HA26NJA.localdomain>
2021-07-01 14:49:14 +0200

    created cart logic to avoid adding the same item twice

--- src/features/cartSlice.js
+++ src/features/cartSlice.js
@@ -2,13 +2,25 @@ import { createSlice } from '@reduxjs/toolkit'
 
 const cartSlice = createSlice({
   name: 'cart',
-  initialState: [],
+  initialState: {
+    currentCart: [],
+  },
   reducers: {
     addToCart(state, action) {
-      state.push(action.payload)
+      const isAlreadyInIndex = state.currentCart.findIndex(
+        item => item.id === action.payload.id,
+      )
+
+      if (isAlreadyInIndex === -1) {
+        state.currentCart = [
+          ...state.currentCart,
+          { ...action.payload, quantity: 1 },
+        ]
+      } else {
+        state.currentCart[isAlreadyInIndex].quantity += 1
+      }
     },
   },
 })
-
 export const { addToCart } = cartSlice.actions
 export default cartSlice.reducer
